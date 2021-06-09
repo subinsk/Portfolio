e671037b633d8d0cdbdb983c760bb81d53082c88
Subin <subinsk284@gmail.com>
2021-06-09 14:11:18 +0530

    done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,4 +9,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,port=6000)
+    app.run(debug=True)
