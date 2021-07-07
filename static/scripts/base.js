65fb134564eaa73d4c840070392b116247211a47
Subin <subinsk284@gmail.com>
2021-07-08 00:52:30 +0530

    some more sections done

---
+++ static/scripts/base.js
@@ -0,0 +1,30 @@
+function Home(){
+    $('html, body').animate({
+        'scrollTop' : $(".home").position().top
+    });
+}
+function About(){
+    $('html, body').animate({
+        'scrollTop' : $(".about").position().top
+    });
+}
+function Skills(){
+    $('html, body').animate({
+        'scrollTop' : $(".skills").position().top
+    });
+}
+function Education(){
+    $('html, body').animate({
+        'scrollTop' : $(".education").position().top
+    });
+}
+function Projects(){
+    $('html, body').animate({
+        'scrollTop' : $(".projects").position().top
+    });
+}
+function Contact(){
+    $('html, body').animate({
+        'scrollTop' : $(".contact").position().top
+    });
+}
