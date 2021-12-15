66ca940499d39ec4842a075931f9aa0a4d554144
Subin <subinsk284@gmail.com>
2021-12-16 02:01:41 +0530

    done

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 def before_request():
     app.jinja_env.cache = {}
+
+
 app.before_request(before_request)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
